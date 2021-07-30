b9618147fa6343db2b93c68329a972aa456b0bb3
Demiar <alexey174@gmail.com>
2021-07-30 12:10:43 +0300

    Initial Commit

---
+++ ChangeColorSwiftUI/ChangeColorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeColorSwiftUIApp.swift
+//  ChangeColorSwiftUI
+//
+//  Created by Алексей on 30.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChangeColorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
